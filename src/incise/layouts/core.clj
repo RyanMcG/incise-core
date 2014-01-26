769963f86b07942674a76017f09e295a54563d11
Ryan McGowan <ryan@ryanmcg.com>
2014-01-25 18:57:22 -0800

    Remove layouts.core dependency on Parse.

--- src/incise/layouts/core.clj
+++ src/incise/layouts/core.clj
@@ -1,6 +1,5 @@
 (ns incise.layouts.core
-  (:require [incise.parsers.parse]
-            [incise.config :as conf])
+  (:require [incise.config :as conf])
   (:refer-clojure :exclude [get]))
 
 (defonce layouts (atom {}))
@@ -14,7 +13,8 @@
   (apply @layouts (name layout-name) more))
 
 (defn Parse->string
-  [^incise.parsers.parse.Parse parse-data]
+  "Take a Parse or parse-like map and pass it through the appropriate layout."
+  [parse-data]
   (if-let [layout-key (:layout parse-data)]
     (if-let [layout-fn (get layout-key)]
       (layout-fn (conf/get) parse-data)
@@ -25,7 +25,12 @@
                      :parse parse-data}))))
 
 (defn register
-  "Register a layout function to a shortname"
+  "Register a layout function to a shortname.
+
+  A layout function takes two arguments. The first is a snapshot of the global
+  configuration. The second is some sort of context map. It should look
+  something like Parse. The only required key is :layout which must be a
+  shortname of a registered layout function."
   [short-names layout-fn]
   (swap! layouts
          merge (zipmap (map name short-names)
