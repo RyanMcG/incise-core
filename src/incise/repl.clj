08b44d7b49f775d1d3cd988e6c60b3fe0e8e17a0
Ryan McGowan <ryan@ryanmcg.com>
2014-02-08 18:39:43 -0800

    Allow timbre configuration through CLI option and config.

--- src/incise/repl.clj
+++ src/incise/repl.clj
@@ -1,14 +1,17 @@
 (ns incise.repl
   (:require [clojure.tools.namespace.repl :refer :all]
-            [incise.server :refer [stop start]]
-            [incise.config :as conf]
-            [incise.core :refer :all]))
+            [taoensso.timbre :as timbre]
+            (incise [server :refer [stop start]]
+                    [config :as conf]
+                    [core :refer :all])))
 
 (defn start-server [& {:as more}]
   (conf/load)
   (conf/merge! {:method :serve
                 :port 5000
                 :thread-count 4} more)
+  (timbre/set-level! (conf/get :log-level))
+  (timbre/merge-config! (conf/get :timbre))
   (start))
 
 (defn restart-server
