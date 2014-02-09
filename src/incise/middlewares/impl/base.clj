c8a3fb0c0b4ff968383622dfe01599f549a2146c
Ryan McGowan <ryan@ryanmcg.com>
2014-02-08 19:17:01 -0800

    Log requests with middleware.

--- src/incise/middlewares/impl/base.clj
+++ src/incise/middlewares/impl/base.clj
@@ -1,8 +1,32 @@
 (ns incise.middlewares.impl.base
   (:require [incise.utils :refer [normalize-uri wrap-log-exceptions]]
+            [taoensso.timbre :refer [color-str info]]
             (ring.middleware [reload :refer [wrap-reload]]
                              [stacktrace :refer [wrap-stacktrace-web]])
             [incise.middlewares.core :refer [register]]))
+
+(def ^:private colors-by-code
+  {2 :green
+   3 :blue
+   4 :yellow
+   5 :red})
+
+(defn- status->color [status]
+  (colors-by-code (int (/ status 100)) :reset))
+
+(defn- color-status [status]
+  (color-str (status->color status)
+             status))
+
+(defn wrap-log-request [handler]
+  (fn [{:keys [uri] :as request}]
+    (let [uniq-id (gensym "REQUEST ")]
+      (info (color-str :white uniq-id " START ┄┅┄ ~") uri)
+      (let [{:keys [status] :as resp} (handler request)]
+        (info (color-str :white uniq-id " END  ")
+               (color-status status)
+               (color-str :white \~) uri)
+        resp))))
 
 (defn wrap-static-index [handler]
   (fn [{:keys [uri] :as request}]
@@ -10,5 +34,6 @@
 
 (register 200 wrap-static-index)
 (register 300 wrap-reload :dirs ["src"])
+(register 700 wrap-log-request)
 (register 800 wrap-log-exceptions)
 (register 900 wrap-stacktrace-web)
