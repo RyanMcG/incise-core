0e34d5dced682d03becae5d8a054a6016467df9b
Ryan McGowan <ryan@ryanmcg.com>
2014-02-14 18:23:04 -0800

    Move test and spec helpers into src.
    This way depdencies can use them too.

--- src/incise/test_helpers.clj
+++ src/incise/test_helpers.clj
@@ -1,9 +1,8 @@
-(ns incise.test-helpers)
+(ns incise.test-helpers
+  (:require [incise.config :as conf]))
 
 (defmacro with-custom-config [custom-config & body]
-  `(with-redefs [incise.config/config
-                 (atom (merge @incise.config/config
-                              ~custom-config))]
+  `(with-redefs [conf/config (atom (merge @conf/config ~custom-config))]
      ~@body))
 
 (defn custom-config-fixturer [& {:as config}]
