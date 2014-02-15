0a7a02d705b7d0d6502208ea32c8e799141daaeb
Ryan McGowan <ryan@ryanmcg.com>
2014-02-14 21:40:14 -0800

    Upgrade depdencies.

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring "1.2.0"]
-                 [compojure "1.1.5"]
-                 [http-kit "2.1.10"]
+                 [ring "1.2.1"]
+                 [compojure "1.1.6"]
+                 [http-kit "2.1.16"]
                  [robert/hooke "1.3.0"]
-                 [org.clojure/java.classpath "0.2.0"]
+                 [org.clojure/java.classpath "0.2.2"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/tools.namespace "0.2.4"]
                  [org.clojure/tools.cli "0.3.1"]
                  [pallet-map-merge "0.1.0"]
-                 [clj-time "0.5.1"]
+                 [clj-time "0.6.0"]
                  [com.taoensso/timbre "3.0.0"]
                  [manners "0.2.0"]]
   :profiles {:dev {:dependencies [[speclj "2.9.1"]]}}
