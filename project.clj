1f21f583984ae87dbe7f120fead5104e956eed99
Ryan McGowan <ryan@ryanmcg.com>
2019-05-23 01:07:23 -0700

    (>'')>

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.4.0"]
+                 [ring "1.7.1"]
                  [com.stuartsierra/component "0.3.0"]
-                 [compojure "1.4.0"]
+                 [compojure "1.6.1"]
                  [http-kit "2.1.19"]
                  [robert/hooke "1.3.0"]
                  [org.clojure/java.classpath "0.3.0"]
@@ -15,7 +15,6 @@
                  [org.clojure/tools.cli "0.3.3"]
                  [pallet-map-merge "0.1.0"]
                  [enlive "1.1.6"]
-                 [clj-time "0.11.0"]
                  [com.taoensso/timbre "3.3.1"]
                  [manners "0.8.0"]
                  [chic-text "0.2.0"]]
